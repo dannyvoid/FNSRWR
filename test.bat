3ffa21da33e138aedd7ed6c5921174b3d6e9e5d8
Danny Void <danielilluminatic@gmail.com>
2017-06-25 13:34:11 -0500

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,31 +1,33 @@
 @echo off
 
-NET SESSION >NUL
-IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE >NUL
+NET SESSION >nul
+IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE >nul
 goto :start
 
 :ELEVATE
 CD /d %~dp0
-MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();" >NUL
+MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();" >nul
 EXIT
 
 :start
-ipconfig /release >NUL
-ipconfig /renew >NUL
-netsh int ip delete arpcache >NUL
-netsh int ip reset >NUL
+ipconfig /release >nul
+ipconfig /renew >nul
+netsh int ip delete arpcache >nul
+netsh int ip reset >nul
+netsh winsock reset >nul
+
 ipconfig /flushdns >nul
 nbtstat -R >nul
 nbtstat -RR >nul
 netsh int reset all >nul
 netsh int ipv4 reset >nul
 netsh int ipv6 reset >nul
-netsh winsock reset >NUL
+netsh winsock reset >nul
 
 for /F "skip=3 tokens=1,2,3* delims= " %%G in ('netsh interface show interface') DO (
     IF "%%H"=="Connected" netsh interface set interface "%%J" disabled
-) >NUL
+) >nul
 
 for /F "skip=3 tokens=1,2,3* delims= " %%G in ('netsh interface show interface') DO (
     IF "%%H"=="Disconnected" netsh interface set interface "%%J" enabled
-) >NUL
+) >nul
