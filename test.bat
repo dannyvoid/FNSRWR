ecec9b0db94b7d098c79892bfb189b8cfdb9244e
Danny Void <danielilluminatic@gmail.com>
2017-06-25 13:32:35 -0500

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,31 @@
+@echo off
+
+NET SESSION >NUL
+IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE >NUL
+goto :start
+
+:ELEVATE
+CD /d %~dp0
+MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();" >NUL
+EXIT
+
+:start
+ipconfig /release >NUL
+ipconfig /renew >NUL
+netsh int ip delete arpcache >NUL
+netsh int ip reset >NUL
+ipconfig /flushdns >nul
+nbtstat -R >nul
+nbtstat -RR >nul
+netsh int reset all >nul
+netsh int ipv4 reset >nul
+netsh int ipv6 reset >nul
+netsh winsock reset >NUL
+
+for /F "skip=3 tokens=1,2,3* delims= " %%G in ('netsh interface show interface') DO (
+    IF "%%H"=="Connected" netsh interface set interface "%%J" disabled
+) >NUL
+
+for /F "skip=3 tokens=1,2,3* delims= " %%G in ('netsh interface show interface') DO (
+    IF "%%H"=="Disconnected" netsh interface set interface "%%J" enabled
+) >NUL
