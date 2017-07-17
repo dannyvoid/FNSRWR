70b779cc3612a6152df7468dcb6ceb88b8a88c8b
Danny Void <danielilluminatic@gmail.com>
2017-07-17 06:18:25 -0500

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,33 +1,59 @@
 @echo off
 
-NET SESSION >nul
-IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE >nul
+NET SESSION 
+IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE 
 goto :start
 
 :ELEVATE
 CD /d %~dp0
-MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();" >nul
+MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();" 
 EXIT
 
 :start
-ipconfig /release >nul
-ipconfig /renew >nul
-netsh int ip delete arpcache >nul
-netsh int ip reset >nul
-netsh winsock reset >nul
+echo ==============================
+echo === FNSRWR v1 by DannyVoid ===
+echo ==============================
+echo.
 
-ipconfig /flushdns >nul
-nbtstat -R >nul
-nbtstat -RR >nul
-netsh int reset all >nul
-netsh int ipv4 reset >nul
-netsh int ipv6 reset >nul
-netsh winsock reset >nul
+:choice
+echo Do you want to continue?
+echo This will temporarily disable internet access!
+set /P c=[Y/N]
+if /I "%c%" EQU "Y" goto :continue
+if /I "%c%" EQU "N" goto :stop
 
+goto :choice
+
+:continue
+echo.
+echo Releasing and Renewing...
+ipconfig /release 
+ipconfig /renew 
+
+echo Resetting Arp Cache...
+netsh int ip delete arpcache 
+
+echo Resetting Local IP...
+netsh int ip reset 
+
+echo Resetting Winsock...
+netsh winsock reset 
+
+echo Resetting Network Adapter...
 for /F "skip=3 tokens=1,2,3* delims= " %%G in ('netsh interface show interface') DO (
     IF "%%H"=="Connected" netsh interface set interface "%%J" disabled
-) >nul
+) 
 
 for /F "skip=3 tokens=1,2,3* delims= " %%G in ('netsh interface show interface') DO (
     IF "%%H"=="Disconnected" netsh interface set interface "%%J" enabled
-) >nul
+) 
+goto :done
+
+:done
+echo.
+echo Complete! Your connection should continue as normal.
+pause
+goto :stop
+
+:stop
+exit
